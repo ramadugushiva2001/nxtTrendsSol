2cde07b5a5410c24f3fb413d1f2dd4b9249f027b
ramadugushiva2001 <ramadugushiva2001@gmail.com>
2024-06-08 06:27:59 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import {Component} from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import CartContext from './context/CartContext'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    cartList: [],
+  }
+
+  componentDidMount() {
+    this.getProductDetails()
+  }
+  //   TODO: Add your code for remove all cart items, increment cart item quantity, decrement cart item quantity, remove cart item
+
+  addCartItem = product => {
+    this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+    //   TODO: Update the code here to implement addCartItem
+  }
+
+  getProductDetails = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    console.log({jwtToken})
+    //const url = 'https://run.mocky.io/v3/947e05e1-cd6a-4af9-93e7-0727fba9fec4'
+
+    const url = 'https://run.mocky.io/v3/8177da5e-b2fd-4474-9bb7-457f4099ae4e'
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(url, options)
+    console.log(response)
+  }
+
+  render() {
+    const {cartList} = this.state
+
+    return (
+      <CartContext.Provider
+        value={{
+          cartList,
+          addCartItem: this.addCartItem,
+          removeCartItem: this.removeCartItem,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={LoginForm} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/products" component={Products} />
+          <ProtectedRoute
+            exact
+            path="/products/:id"
+            component={ProductItemDetails}
+          />
+          <ProtectedRoute exact path="/cart" component={Cart} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="not-found" />
+        </Switch>
+      </CartContext.Provider>
+    )
+  }
+}
+
+export default App
